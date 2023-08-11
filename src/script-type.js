554e750a2500d6e177e1bdf06b639a03033c527c
ardiansyahx1 <141700510+ardiansyahx1@users.noreply.github.com>
2023-08-11 20:24:46 +0800

    Update script-type.js

--- src/script-type.js
+++ src/script-type.js
@@ -3,7 +3,7 @@
 
 
 const typed = new Typed('.multiple-text', {
-strings: ['Fronted Newbie','Student','Desinger'],
+strings: ['Fronted Newbie','Student','Designer'],
 typeSpeed: 100,
 backSpeed: 100,
 backDelay: 1000,
